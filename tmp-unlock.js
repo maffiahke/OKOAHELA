9d66e171c09ea8c09aba52191c668b0f985467b3
maffiahke <techjei02@gmail.com>
2026-09-12 11:23:31 +0300

    Save uncommitted changes

--- tmp-unlock.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const p = new PrismaClient();
-p.loanProduct.updateMany({ data: { minSavings: 0 } })
-  .then((r) => { console.log("minSavings=0 on " + r.count + " products"); return p.loanProduct.count({ where: { minSavings: { gt: 0 } } }); })
-  .then((left) => { console.log("products still requiring savings: " + left); })
-  .finally(() => p.$disconnect());
